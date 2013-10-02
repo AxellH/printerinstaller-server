e2058678dfe622a002dd88cf558227d2d45a99ce
Eldon Ahrold <eldon.ahrold@gmail.com>
2013-10-02 10:54:59 -0500

    tweaks for os x server web app

--- OSX/printerinstaller_example.wsgi
+++ OSX/printerinstaller_example.wsgi
@@ -2,7 +2,7 @@ import os, sys
 import site
 
 #set the next line to your printerinstaller environment
-VIR_ENV_DIR = '/path/to/your/printerinstaller_env/printerinstaller-server/'
+VIR_ENV_DIR = '/usr/local/www/printerinstaller_env'
 
 # Use site to load the site-packages directory of our virtualenv
 site.addsitedir(os.path.join(VIR_ENV_DIR, 'lib/python2.7/site-packages'))
--- printerinstaller/urls.py
+++ printerinstaller/urls.py
@@ -6,10 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
-    url(r'^printers/admin/', include(admin.site.urls)),
-    url(r'^printers/login/$', 'django.contrib.auth.views.login',name='login'),
-    url(r'^printers/logout/$', 'django.contrib.auth.views.logout_then_login',name='logout'),
-    url(r'^printers/changepassword/$', 'django.contrib.auth.views.password_change',name='change_password'),
-    url(r'^printers/changepassword/done/$', 'django.contrib.auth.views.password_change_done'),
-    url(r'^printers/', include('printers.urls'),name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login',name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',name='logout'),
+    url(r'^changepassword/$', 'django.contrib.auth.views.password_change',name='change_password'),
+    url(r'^changepassword/done/$', 'django.contrib.auth.views.password_change_done'),
+    url(r'^', include('printers.urls'),name='home'),
 )
